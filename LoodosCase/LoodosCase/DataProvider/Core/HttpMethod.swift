fb3bf31d2b6461fdd9db75788321697e04f08f1f
Gizem Duman <45661081+dumanngizem@users.noreply.github.com>
2023-05-27 14:09:52 +0300

    LC-003 create dataProvider

---
+++ LoodosCase/LoodosCase/DataProvider/Core/HttpMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HttpMethod.swift
+//  LoodosCase
+//
+//  Created by Gizem Duman on 27.05.2023.
+//
+
+import Foundation
+
+enum HttpMethod : String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+    case put = "PUT"
+}
